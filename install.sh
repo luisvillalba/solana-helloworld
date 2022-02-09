f09f0e2d3b9b2c2a4df19cc9e2862b105f4191c5
Luis M. Villalba <luismvillalba@gmail.com>
2022-02-09 01:51:03 +0000

    Updated

--- install.sh
+++
@@ -1,3 +0,0 @@
-sh -c "$(curl -sSfL https://release.solana.com/v1.9.5/install)";
-cp -R .solana-config/* /home/gitpod/.config/solana/;
-echo "export PATH='/home/gitpod/.local/share/solana/install/active_release/bin:\\$PATH'" >> /home/gitpod/.bashrc;
