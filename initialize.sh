3990bddc8ff9cc31ad90845daacbfdec3e74bd20
Luis M. Villalba <luismvillalba@gmail.com>
2022-02-10 01:47:27 +0000

    Updated

--- initialize.sh
+++
@@ -1,5 +0,0 @@
-sh -c "$(curl -sSfL https://release.solana.com/v1.9.5/install)";
-rm -rf /home/gitpod/.config/solana/*;
-cp -R /workspace/solana-helloworld/.solana-config/* /home/gitpod/.config/solana/;
-gp env PATH="/home/gitpod/.local/share/solana/install/active_release/bin:$PATH"
-solana config set --url localhost
---
+++ install-sol.sh
@@ -0,0 +1,2 @@
+sh -c "$(curl -sSfL https://release.solana.com/v1.9.5/install)";
+gp env PATH="/home/gitpod/.local/share/solana/install/active_release/bin:$PATH"
