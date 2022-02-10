7116842a234c617fd7413cb4dc2b0422d1d95462
Luis M. Villalba <luismvillalba@gmail.com>
2022-02-10 01:12:31 +0000

    Updated

--- client/index.ts
+++ client/index.ts
@@ -8,7 +8,7 @@ import {
   Transaction,
   sendAndConfirmTransaction,
 } from '@solana/web3.js';
-
+import * as borsh from 'borsh';
 import * as fs from 'fs';
 const seed: string = fs.readFileSync('/home/gitpod/.config/solana/id.json').toString();
 const programId: PublicKey = new PublicKey(fs.readFileSync('./programid.txt').toString().trim());
@@ -26,15 +26,93 @@ async function establishConnection(): Promise<Connection> {
   return connection;
 }
 
+class InstructionInfo {
+  amount = 0;
+  constructor(fields: {amount: number} | undefined = undefined) {
+    if (fields) {
+      this.amount = fields.amount;
+    }
+  }
+}
+
+/**
+ * Borsh schema definition for greeting accounts
+ */
+const InstructionInfoSchema = new Map([
+  [InstructionInfo, {kind: 'struct', fields: [['amount', 'u32']]}],
+]);
+
+/**
+ * The state of a greeting account managed by the hello world program
+ */
+ class StateAccount {
+  counter = 0;
+  balance = 0;
+  constructor(fields: {counter: number, balance: number} | undefined = undefined) {
+    if (fields) {
+      this.counter = fields.counter;
+      this.balance = fields.balance;
+    }
+  }
+}
+
+/**
+ * Borsh schema definition for greeting accounts
+ */
+const GreetingSchema = new Map([
+  [StateAccount, {kind: 'struct', fields: [['counter', 'u32'], ['balance', 'u32']]}],
+]);
+
+/**
+ * The expected size of each greeting account.
+ */
+const GREETING_SIZE = borsh.serialize(
+  GreetingSchema,
+  new StateAccount(),
+).length;
+
+const getAccount = async (connection: Connection, signer: Keypair): Promise<PublicKey> => {
+  // Derive the address (public key) of a greeting account from the program so that it's easy to find later.
+  const programAccountSeed = 'hello22sss';
+  const programAccountKey = await PublicKey.createWithSeed(
+    signer.publicKey,
+    programAccountSeed,
+    programId,
+  );
+  // Check if the greeting account has already been created
+  const programAccount = await connection.getAccountInfo(programAccountKey);
+
+  if (programAccount === null) {
+    console.log("Creating new program account", signer.publicKey);
+
+    const transaction = new Transaction().add(
+      SystemProgram.createAccountWithSeed({
+        fromPubkey: signer.publicKey,
+        basePubkey: signer.publicKey,
+        seed: programAccountSeed,
+        newAccountPubkey: programAccountKey,
+        lamports: 400000000000000,
+        space: GREETING_SIZE,
+        programId,
+      }),
+    );
+    await sendAndConfirmTransaction(connection, transaction, [signer]);
+  }
+  return programAccountKey;
+}
+
 const main = async () => {
   const signer: Keypair = Keypair.fromSecretKey(key);
   const connection = await establishConnection();
   const balance = await connection.getBalance(signer.publicKey);
+  const programAccount = await getAccount(connection, signer);
 
   const instruction = new TransactionInstruction({
-    keys: [{ pubkey: signer.publicKey, isSigner: false, isWritable: true }],
+    keys: [{ pubkey: programAccount, isSigner: false, isWritable: true }],
     programId,
-    data: Buffer.from('123'),
+    data: Buffer.from(borsh.serialize(InstructionInfoSchema, new InstructionInfo({
+      amount: 100
+    }))),
   });
   await sendAndConfirmTransaction(
     connection,
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,52 @@
+use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+    account_info::{AccountInfo, next_account_info}, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey, program_error::ProgramError
 };
 
 entrypoint!(process_instruction);
+
+/// Define the type of state stored in accounts
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct StateAccount {
+    /// number of greetings
+    pub counter: u32,
+    pub balance: u32,
+}
+
+/// Define the type of state stored in accounts
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct InputData {
+    pub amount: u32,
+}
 
 fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    msg!(
-        "Process_instruction: {}: {} accounts, data={:?}",
-        program_id,
-        accounts.len(),
-        instruction_data
-    );
+    msg!("Hello World Rust program entrypoint");
+    let data: InputData = InputData::try_from_slice(&instruction_data)?;
+    let accounts_iter = &mut accounts.iter();
+    let account = next_account_info(accounts_iter)?;
+
+    msg!("Adding: {}", data.amount);
+
+    msg!("account.owner: {}", account.owner);
+    msg!("account.key: {}", account.key);
+
+    if account.owner != program_id {
+        msg!("Greeted account does not have the correct program id");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    // // Increment and store the number of times the account has been greeted
+    let mut greeting_account: StateAccount = StateAccount::try_from_slice(&account.data.borrow())?;
+    greeting_account.counter += 1;
+    greeting_account.balance += data.amount;
+    greeting_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
+
+    msg!("Now the counter is: {}", greeting_account.counter);
+    msg!("Now the balance is is: {}", greeting_account.balance);
 
     Ok(())
 }
