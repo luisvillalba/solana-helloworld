0e85c8765b444c51ae90951b84af896e27e5cd4e
Luis M. Villalba <luismvillalba@gmail.com>
2022-02-10 02:13:42 +0000

    fixed

--- install-sol.sh
+++ install-sol.sh
@@ -1,2 +1,13 @@
 sh -c "$(curl -sSfL https://release.solana.com/v1.9.5/install)";
-gp env PATH="/home/gitpod/.local/share/solana/install/active_release/bin:$PATH"
+
+if [[ "$PATH" != .*"solana/install/active_release/bin".* ]]; then
+  gp env PATH="/home/gitpod/.local/share/solana/install/active_release/bin:$PATH"
+fi
+
+if [ -f "./.solana-config/id.json" ]; then
+    rm -rf /home/gitpod/.config/solana/*;
+    cp -R /workspace/solana-helloworld/.solana-config/* /home/gitpod/.config/solana/;
+else 
+    solana-keygen new --no-bip39-passphrase
+    cp -r /home/gitpod/.config/solana/* /workspace/solana-helloworld/.solana-config/
+fi
--- recover-config.sh
+++ recover-config.sh
@@ -1,9 +1,13 @@
+if [[ "$PATH" != .*"solana/install/active_release/bin".* ]]; then
+  gp env PATH="/home/gitpod/.local/share/solana/install/active_release/bin:$PATH"
+fi
+
 if [ -f "./.solana-config/id.json" ]; then
     rm -rf /home/gitpod/.config/solana/*;
     cp -R /workspace/solana-helloworld/.solana-config/* /home/gitpod/.config/solana/;
 else 
     solana-keygen new --no-bip39-passphrase
-    cp -r /home/gitpod/.config/solana/* ./.solana-config
+    cp -r /home/gitpod/.config/solana/* /workspace/solana-helloworld/.solana-config/
 fi
 
 solana config set --url localhost
