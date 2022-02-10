9883e33d0d51f12a29f175b92d8aa648821052b0
Luis M. Villalba <luismvillalba@gmail.com>
2022-02-10 02:21:29 +0000

    FIxed path

--- install-sol.sh
+++ install-sol.sh
@@ -1,7 +1,7 @@
 sh -c "$(curl -sSfL https://release.solana.com/v1.9.5/install)";
 
-if [[ "$PATH" != .*"solana/install/active_release/bin".* ]]; then
-  gp env PATH="/home/gitpod/.local/share/solana/install/active_release/bin:$PATH"
+if [[ ! "$PATH" =~ .*"solana/install/active_release/bin".* ]]; then
+    gp env PATH="/home/gitpod/.local/share/solana/install/active_release/bin:$PATH"
 fi
 
 if [ -f "./.solana-config/id.json" ]; then
--- recover-config.sh
+++ recover-config.sh
@@ -1,5 +1,5 @@
-if [[ "$PATH" != .*"solana/install/active_release/bin".* ]]; then
-  gp env PATH="/home/gitpod/.local/share/solana/install/active_release/bin:$PATH"
+if [[ ! "$PATH" =~ .*"solana/install/active_release/bin".* ]]; then
+    gp env PATH="/home/gitpod/.local/share/solana/install/active_release/bin:$PATH"
 fi
 
 if [ -f "./.solana-config/id.json" ]; then
